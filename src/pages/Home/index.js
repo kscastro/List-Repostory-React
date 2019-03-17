c5ba31ecfca0f89f193877a4f7633322512d59ca
Kaike Souto <kaike.souto@bemobi.com.br>
2019-03-17 19:25:32 -0300

    First Commit

---
+++ src/pages/Home/index.js
@@ -0,0 +1,48 @@
+import React, { Component } from 'react';
+import moment from 'moment';
+import logo from '../../assets/logo.png';
+import { Container, Form } from './styles';
+import CompareList from '../../components/CompareList';
+import Api from '../../services/api';
+
+export default class Home extends Component {
+  state = {
+    repositoryInput: '',
+    repositories: [],
+  };
+
+  handleAddRepository = async (e) => {
+    e.preventDefault();
+
+    try {
+      const { data: repository } = await Api.get(`/repos/${this.state.repositoryInput}`);
+      repository.lastCommit = moment(repository.pushed_at).fromNow();
+
+      this.setState({
+        repositoryInput: '',
+        repositories: [...this.state.repositories, repository],
+      });
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  render() {
+    return (
+      <Container>
+        <img src={logo} alt="Github Compare" />
+
+        <Form onSubmit={this.handleAddRepository}>
+          <input
+            type="text"
+            placeholder="usuário/repositório"
+            value={this.state.repositoryInput}
+            onChange={e => this.setState({ repositoryInput: e.target.value })}
+          />
+          <button type="submit">OK</button>
+        </Form>
+        <CompareList repositories={this.state.repositories} />
+      </Container>
+    );
+  }
+}
